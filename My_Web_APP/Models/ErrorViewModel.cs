4dab77f971ba8dbc202133fc7a93db5dac1d7fdb
jagadeesh777447 <jagadeesh.pinninti@technoxis.in>
2026-01-02 18:06:42 +0530

    Initial commit - ASP.NET Core project

---
+++ My_Web_APP/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace My_Web_APP.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
